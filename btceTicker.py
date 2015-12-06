f83c039a86a957e9e1bc5c9c6bca5581072981cf
ggs134 <inditow@gmail.com>
2015-12-07 01:24:14 +0900

    btceTicker에 코빗 추가

--- btceTicker.py
+++ btceTicker.py
@@ -2,18 +2,29 @@ import requests
 import pymongo
 import json
 import time
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 
 def insertData():
-    cli = pymongo.MongoClient("52.192.242.11", 27017)
+    cli = pymongo.MongoClient("192.168.1.19", 27017)
     #cli = pymongo.MongoClient()
     db = cli.ticker
+    
+    #Get Btc-e Data
+    btceResult = requests.get('https://btc-e.com/api/3/ticker/btc_usd')
+    #Get Korbit Data
+    korbitResult = requests.get('https://api.korbit.co.kr/v1/ticker/detailed')
 
-    res = requests.get('https://btc-e.com/api/3/ticker/btc_usd')
-    data = json.loads(res.text)
-    data['exchangeRate'] = getExRate()
-    db.btce.insert(data)
-    print data
+    #parse data
+    btceData = json.loads(korbitResult.text)
+    korbitData = json.loads(btceResult.text)
+    
+    result = {}
+    
+    result['korbit']=korbitData
+    result['btce']=btceData
+    result['time']=time.time()    
+    db.btce.insert(result)
+    print result
 
 def getExRate():
     exch = requests.get('http://community.fxkeb.com/fxportal/jsp/RS/DEPLOY_E    XRATE/fxrate_all.html')
@@ -22,9 +33,12 @@ def getExRate():
     return float(data)
 
 def main():
-    while True:
-        insertData()
-        time.sleep(60)
+    try:
+        while True:
+            insertData()
+            time.sleep(20)
+    except Exception as e:
+        print e
 
 if __name__=="__main__":
     main()
